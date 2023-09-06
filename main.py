2c8d28662af6eb9235a776a04ccdeb6620d13519
h7amod <ma.albuti@gmail.com>
2023-09-06 19:14:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # write your code here
+def check_hobbies(person):
+    if len(person['hobbies']) > 3:
+        print('WOW YOU ARE AMAZING')
+
+if __name__ == "__main__":
+
+    person = {
+        'name': 'John',
+        'age': 30,
+        'hobbies': ['reading', 'swimming', 'cooking']
+    }
+
+    print(person['name'])
+    print(person['age'])
+
+    person['age'] = 35
+    person['country'] = 'USA'
+    print(person)
+    print(len(person))
+
+    person['hobbies'].append('painting')
+    check_hobbies(person)
